5d2599c7a74c6c64817c81328f67be3e32697c6b
Conner <cpetzold@gmail.com>
2010-09-03 01:06:29 -0700

    testing git-hook auto deployment!

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/', function(req, res){
   res.render('index.jade', {
     locals: {
       site: sitename,
-      title: '',
+      title: 'home',
       subtitle: '',
       bodyid: 'home'
     }
