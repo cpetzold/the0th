5d81f55d36d6bf8b1d2b18a9e605384e710f3e86
Conner <cpetzold@gmail.com>
2010-11-05 21:36:20 -0700

    started mongoose stuff

--- app.js
+++ app.js
@@ -1,37 +1,29 @@
-
-/**
- * Module dependencies.
- */
-
 var sys = require('sys'),
     express = require('express'),
-    connect = require('connect');
+    mongoose = require('mongoose').Mongoose;
 
 var app = module.exports = express.createServer();
 
-// Configuration
-
 app.configure(function(){
     app.set('views', __dirname + '/views');
-    app.use(connect.bodyDecoder());
-    app.use(connect.methodOverride());
-    app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
+    app.use(express.bodyDecoder());
+    app.use(express.methodOverride());
+    app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
     app.use(app.router);
-    app.use(connect.staticProvider(__dirname + '/public'));
+    app.use(express.staticProvider(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-   app.use(connect.errorHandler()); 
+   app.use(express.errorHandler()); 
 });
 
 var sitename = 'the0th';
 var pages = ['résumé', 'resume', 'projects'];
 
-// Routes
 app.get('/:page', function(req, res) {
   var pagename = decodeURIComponent(req.params.page);
   pagename = (pagename == 'résumé') ? 'resume' : pagename;
@@ -54,7 +46,5 @@ app.get('/', function(req, res){
     }
   });
 });
-
-
 
 if (!module.parent) app.listen(8000);
