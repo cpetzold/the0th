c51456747a2af91b9954a455cf2062f641cdf20d
Conner <cpetzold@gmail.com>
2010-11-09 08:08:36 -0800

    rearranged models, got user model working and started playing with it

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var sys = require('sys'),
+    ext = require('ext'),
     express = require('express'),
-    mongoose = require('mongoose').Mongoose;
+    mongoose = require('mongoose').Mongoose,
+    models = require('./lib/models');
 
 var app = module.exports = express.createServer();
+var db = mongoose.connect('mongodb://localhost/the0th');
+
+var User = db.model('User'),
+    Post = db.model('Post');
+    
+var u = new User({
+  'username':'cpetzold',
+  'password':'test'
+  'name':'conner petzold',
+  'email':'cpetzold@gmail.com',
+  'type':'admin',
+  
+});
+u.save(function(){
+  console.log('Conner was saved!');
+});
 
 app.configure(function(){
     app.set('views', __dirname + '/views');
@@ -40,9 +58,9 @@ app.get('/', function(req, res){
   res.render('index.jade', {
     locals: {
       site: sitename,
-      title: 'woot!',
-      subtitle: '',
-      bodyid: 'home'
+      title: 'Blog',
+      subtitle: 'Ohhhh yeah',
+      bodyid: 'blog'
     }
   });
 });
