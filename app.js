e54eb5ffcf11461c449ef87185bc76eb741c6088
Conner <cpetzold@gmail.com>
2010-11-09 19:08:11 -0800

    added login logic with sessions

--- app.js
+++ app.js
@@ -10,21 +10,23 @@ var db = mongoose.connect('mongodb://localhost/the0th');
 var User = db.model('User'),
     Post = db.model('Post');
     
-var u = new User({
-  'username':'cpetzold',
-  'password':'test',
-  'name':'conner petzold',
-  'email':'cpetzold@gmail.com',
-  'type':'admin',
-  
-});
-u.save(function(){
-  console.log('Conner was saved!');
-});
+// var u = new User({
+//   'username':'cpetzold',
+//   'password':'test',
+//   'name':'conner petzold',
+//   'email':'cpetzold@gmail.com',
+//   'type':'admin'
+// });
+// 
+// u.save(function(){
+//   console.log('Conner was saved!');
+// });
 
 app.configure(function(){
     app.set('views', __dirname + '/views');
     app.use(express.bodyDecoder());
+    app.use(express.cookieDecoder());
+    app.use(express.session());
     app.use(express.methodOverride());
     app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
     app.use(app.router);
@@ -54,7 +56,27 @@ app.get('/:page', function(req, res) {
   });
 });
 
+/* Login.. to a snowman */
+app.post('/login', function(req, res){  
+  var u = req.body.username,
+      p = req.body.password;
+  
+  User.authenticate(u, p, function(o){
+    if (!o.error) {
+      req.session.userid = o.id;
+      res.send({'username': o.username}, 200);
+    } else {
+      res.send(o, 404);
+    }
+  });  
+});
+
 app.get('/', function(req, res){
+  
+  User.find({'id':req.session.userid}).first(function(user){
+    console.log('user:', sys.inspect(user));
+  });
+  
   res.render('index.jade', {
     locals: {
       site: sitename,
