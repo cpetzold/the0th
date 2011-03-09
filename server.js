f12f63a8484431bafbbb5da7156cc5e5e1da1879
Conner <cpetzold@gmail.com>
2011-03-09 10:19:40 -0800

    switched to stylus, changed design

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var express = require('express'),
+    stylus = require('stylus'),
+    log = require('logging').from(__filename),
     fs = require('fs');
+
+Number.prototype.toPhone = function(){
+  var str = this.toString();
+  return str.substr(0,3) + '.' + str.substr(3,3) + '.' + str.substr(6,4);
+}
 
 var server = module.exports = express.createServer();
 
@@ -7,24 +14,49 @@ server.configure(function(){
   server.set('views', __dirname + '/views');
   server.use(express.methodOverride());
   server.use(server.router);
-  server.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
-  server.use(express.staticProvider(__dirname + '/public'));
+  server.use(stylus.middleware(__dirname + '/public'));
+  server.use(express.static(__dirname + '/public'));
 });
 
 server.get('/', function(req, res){
-  res.render('index.jade', {
-    locals: {
-      title: 'Conner Petzold',
-      subtitle: 'Web developer at Yahoo!'
+  fs.readFile('resume.json', 'utf8', function(err, resume){
+    res.render('index.jade', {
+      locals: {
+        title: 'Conner Petzold',
+        subtitle: 'Web developer',
+        nav: [
+          { name: 'résumé', path: '/résumé' },
+          // { name: 'projects', path: '/projects' },
+          { name: 'github', path: 'github.com/', username: 'cpetzold' },
+          { name: 'twitter', path: 'twitter.com/', username: 'cpetzold' },
+          { name: 'linkedin', path: 'linkedin.com/in/', username: 'cpetzold' },
+          { name: 'facebook', path: 'facebook.com/', username: 'conner' }
+        ]
+      }
+    });
+  });
+});
+
+server.get('/:resume.:format?', function(req, res, next){
+  if (req.params.resume != 'résumé' && req.params.resume != 'resume') return next();
+  fs.readFile('resume.json', 'utf8', function(err, resume){
+    if (!err) {
+      if (req.params.format == 'json') {
+        res.send(resume, { 'Content-Type': 'application/json' });
+      } else {
+        res.render('resume.jade', {
+          locals: {
+            title: 'Conner Petzold - Résumé',
+            resume: JSON.parse(resume)
+          }
+        });
+      }
     }
   });
 });
 
-server.get('/:resume', function(req, res, next){
-  if (req.params.resume != 'résumé' && req.params.resume != 'resume') return next();
-  fs.readFile('resume.json', 'utf8', function(err, resume){
-    if (!err) res.send(resume, { 'Content-Type': 'application/json' });
-  });
-});
-
-if (!module.parent) server.listen(process.env.PORT || 8080);
+if (!module.parent) {
+  var port = process.env.PORT || 8080;
+  server.listen(port);
+  log('listening on port', port);
+}
